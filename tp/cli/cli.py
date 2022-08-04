Directional average default change, bug fixes
"average" is now "norm", old behaviour can be accessed with "mean"
"norm" is unchanged

tp/cli/cli.py
@@ -670,7 +670,7 @@ def plot():
               default='both',
               show_default=True)
 @click.option('--crt',
-        help='Constant relaxation time value.  [default: off]',
+        help='Constant relaxation time rate.  [default: off]',
               type=float)
 @doping_option
 @temperature_option
@@ -730,10 +730,6 @@ def avg_rates(filenames, total, x, crt, doping, temperature, colour, linestyle,
         raise Exception('Please specify at most two filenames')
 
     nlines = np.amax([len(d['stype']) for d in data])
-    if total:
-        nlines += 1
-    if crt is not None:
-        nlines += 1
 
     linestyle = list(linestyle)
     colour = list(colour)
@@ -766,14 +762,10 @@ def avg_rates(filenames, total, x, crt, doping, temperature, colour, linestyle,
             print('Using {} {}.'.format(tdata['meta']['doping'],
                                         tdata['meta']['units']['doping']))
         for i, rate in enumerate(data[tindex]['stype']):
-            tax.plot(tdata['temperature'], tdata['weighted_rates'][i],
-                     color=colours[i], linestyle=linestyle[i],
-                     marker=marker[i], label=rate)
-        if total:
-            i += 1
-            totrate = np.sum(tdata['weighted_rates'], axis=0)
-            tax.plot(tdata['temperature'], totrate, color=colours[i],
-                     linestyle=linestyle[i], marker=marker[i], label='Total')
+            if total or rate != 'Total':
+                tax.plot(tdata['temperature'], tdata['weighted_rates'][i],
+                         color=colours[i], linestyle=linestyle[i],
+                         marker=marker[i], label=rate)
         if crt is not None:
             i += 1
             crtrate = np.full(len(tdata['temperature']), crt)
@@ -790,14 +782,10 @@ def avg_rates(filenames, total, x, crt, doping, temperature, colour, linestyle,
             print('Using {} {}.'.format(ddata['meta']['temperature'],
                                         ddata['meta']['units']['temperature']))
         for i, rate in enumerate(data[dindex]['stype']):
-            dax.plot(np.abs(ddata['doping']), ddata['weighted_rates'][i],
-                     color=colours[i], linestyle=linestyle[i],
-                     marker=marker[i], label=rate)
-        if total:
-            i += 1
-            totrate = np.sum(ddata['weighted_rates'], axis=0)
-            dax.plot(np.abs(ddata['doping']), totrate, color=colours[i],
-                     linestyle=linestyle[i], marker=marker[i], label='Total')
+            if total or rate != 'Total':
+                dax.plot(np.abs(ddata['doping']), ddata['weighted_rates'][i],
+                         color=colours[i], linestyle=linestyle[i],
+                         marker=marker[i], label=rate)
         if crt is not None:
             i += 1
             crtrate = np.full(len(ddata['doping']), crt)
@@ -1566,6 +1554,8 @@ def transport(filenames, kfile, quantity, direction, tmin, tmax, dtype, doping,
         else:
             raise Exception('--kfile must be specified for a '
                             '--quantity of {} or {}.'.format(ltc, tc))
+    else:
+        kdata = None
 
     for e in edata:
         e['doping'] = np.abs(e['doping'])

tp/data/load.py
@@ -136,7 +136,7 @@ def amset(filename, quantities=['seebeck', 'conductivity',
                     data2[q2] = np.swapaxes(data2[q2],0,1)
         if q2 in dimensions and 'stype' in dimensions[q2]:
             if q2 == 'mobility':
-                data[q2]['total'] = data[q2].pop('overall')
+                data[q2]['Total'] = data[q2].pop('overall')
             if 'stype' not in data2:
                 data2['stype'] = list(data[q].keys())
             # for consistency with the format in the mesh data
@@ -158,7 +158,7 @@ def amset(filename, quantities=['seebeck', 'conductivity',
 
     for c in conversions:
         if c in data2:
-            data2[c] = np.multiply(data2[c], conversions[c])
+            data2[c] = np.multiply(data2[c], float(conversions[c]))
 
     return data2
 
@@ -356,7 +356,7 @@ def amset_mesh(filename, quantities='scattering_rates', doping='n',
 
     if 'stype' in data:
         data['stype'] = [l.decode('UTF-8') for l in data['stype']]
-        data['stype'].append('total')
+        data['stype'].append('Total')
 
     for c in aconversions:
         if c in data:

tp/data/resolve.py
@@ -34,7 +34,7 @@ def resolve(data, quantities, **kwargs):
 
                 direction
                     direction to resolve, accepts x-z/, a-c,
-                    average/ avg or normal/ norm.
+                    average/ avg/ normal/ norm or mean.
                 dtype
                     n or p
                 stype
@@ -122,7 +122,7 @@ def resolve(data, quantities, **kwargs):
                             data[q] = np.moveaxis(data[q], i, 0)
                             if val in direction:
                                 data[q] = data[q][direction[val]]
-                            elif val in ['avg', 'average']:
+                            elif val in ['mean']:
                                 if len(data['meta']['dimensions'][q]) > i and \
                                    data['meta']['dimensions'][q][i] == 3:
                                     # if this is a 3x3 array
@@ -135,8 +135,9 @@ def resolve(data, quantities, **kwargs):
                                 else:
                                     # if this is a 3x1 or 6x1 array
                                     data[q] = np.average(data[q][:3], axis=0)
-                            elif val in ['norm', 'normal']:
-                                if data['meta']['dimensions'][q][i] == 3:
+                            elif val in ['avg', 'average', 'norm', 'normal']:
+                                if len(data['meta']['dimensions'][q]) > i and \
+                                   data['meta']['dimensions'][q][i] == 3:
                                     # if this is a 3x3 array
                                     del data['meta']['dimensions'][q][i]
                                     data[q] = np.moveaxis(data[q], i+1, 1)